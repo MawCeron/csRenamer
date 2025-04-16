b983fcedc526107e04b57934a0cd1460be0531af
Mauricio Ceron <ceron.maw@gmail.com>
2025-04-16 09:59:40 -0600

    Add license info and versioning to AboutWindow
    - Implemented a new `License_Click` method in `AboutWindow.xaml.cs` to display the MIT License.
    - Updated the button in `AboutWindow.xaml` to trigger the new license method and modified the message box title.
    - Added versioning information to `csRenamer.csproj`, including `Version`, `AssemblyVersion`, `FileVersion`, and `InformationalVersion`, all set to "0.1.0".

--- csRenamer/Views/AboutWindow.xaml.cs
+++ csRenamer/Views/AboutWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,6 +26,14 @@ namespace csRenamer.Views
             InitializeComponent();
             Owner = owner;
             WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
+            var version = Assembly.GetExecutingAssembly()
+                              .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                              ?.InformationalVersion ?? "Unknown";
+
+            versionText.Text = $"ver. {version.Split('+')[0]}";
+            commitText.Text = $"commit {version.Split('+')[1]}";
+            Debug.WriteLine($"Version: {version}");
         }
 
         private void Issue_Click(object sender, RoutedEventArgs e)
@@ -61,7 +70,22 @@ Copyright © 2006-2008 Adolfo González Blázquez <code@infinicode.org>
 Licensed under the GNU General Public License v2.0.  
 Repository: https://github.com/tfree87/pyRenamer/";
 
-            MessageBox.Show(aboutText, "About csRenamer", MessageBoxButton.OK);
+            MessageBox.Show(aboutText, "About csRenamer - Credits", MessageBoxButton.OK);
+        }
+
+        private void License_Click(object sender, RoutedEventArgs e)
+        {
+            string aboutText = @"MIT License
+
+Copyright © 2025 Mauricio Cerón Medina
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+
+            MessageBox.Show(aboutText, "About csRenamer - License", MessageBoxButton.OK);
         }
     }
 }
